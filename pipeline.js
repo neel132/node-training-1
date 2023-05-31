6b4d40bd4e360501642438e7cc0ab0bf3800c6e6
Apple <apple@Apples-MacBook-Pro.local>
2023-05-31 09:18:55 +0530

    Comments added for chaining pipeline

--- pipeline.js
+++ pipeline.js
@@ -3,10 +3,11 @@ const fs = require('node:fs');
 const zlib = require('node:zlib');
 
 async function run() {
+	// Chaining streams within pipeline
 	await pipeline(
-		fs.createReadStream('archive.tar'),
-		zlib.createGzip(),
-		fs.createWriteStream('archive.tar.gz'),
+		fs.createReadStream('archive.tar'), // Output for this is the input for the next
+		zlib.createGzip(), // Output for this is the input for the next and input of this is the output of the previous stream
+		fs.createWriteStream('archive.tar.gz'), // Input for this is the output of the previous stream
 	);
 	console.log("Pipeline executed successfully...");
 }
